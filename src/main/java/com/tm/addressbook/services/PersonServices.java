e1071b8d52c26071ae41852d756032fb06caf264
Aktorius <miri.tarik@gmail.com>
2017-05-08 18:56:56 +0100

    Implement the contracts exposed in IPersonServices

--- src/main/java/com/tm/addressbook/services/PersonServices.java
+++ src/main/java/com/tm/addressbook/services/PersonServices.java
@@ -3,12 +3,19 @@ package main.java.com.tm.addressbook.services;
 import main.java.com.tm.addressbook.contracts.IPersonServices;
 import main.java.com.tm.addressbook.domain.Person;
 
+import java.time.Period;
+
 /**
  * Created by Aktorius on 08/05/2017.
+ * Class that implements the contracts exposed in IPersonServices
  */
 public class PersonServices implements IPersonServices {
     @Override
     public int findAgeDifferenceInDaysBetween(Person firstPerson, Person otherPerson) {
-        return 0;
+        if (firstPerson == null) throw new IllegalArgumentException("firstPerson cannot be null");
+        if(otherPerson == null) throw new IllegalArgumentException("otherPerson cannot be null");
+
+        Period period = Period.between(firstPerson.getDateOfBirth(), otherPerson.getDateOfBirth());
+        return period.getDays();
     }
 }
