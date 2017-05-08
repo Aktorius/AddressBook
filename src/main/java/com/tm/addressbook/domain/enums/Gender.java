fac2906ec426fc60f633a0f47933824fa005d526
Aktorius <miri.tarik@gmail.com>
2017-05-08 18:20:42 +0100

    Create the definition of a Person

---
+++ src/main/java/com/tm/addressbook/domain/enums/Gender.java
@@ -0,0 +1,10 @@
+package main.java.com.tm.addressbook.domain.enums;
+
+/**
+ * Created by Aktorius on 08/05/
+ * Enum defining the possible genders for a Person
+ */
+public enum Gender {
+    MALE,
+    FEMALE
+}
