ef65204ae35ce58d407eb12ce37f9a7634643683
Aktorius <miri.tarik@gmail.com>
2017-05-08 18:45:42 +0100

    Add class PersonServices that will contain the implementation of the contract IPersonServices

---
+++ src/main/java/com/tm/addressbook/services/PersonServices.java
@@ -0,0 +1,14 @@
+package main.java.com.tm.addressbook.services;
+
+import main.java.com.tm.addressbook.contracts.IPersonServices;
+import main.java.com.tm.addressbook.domain.Person;
+
+/**
+ * Created by Aktorius on 08/05/2017.
+ */
+public class PersonServices implements IPersonServices {
+    @Override
+    public int findAgeDifferenceInDaysBetween(Person firstPerson, Person otherPerson) {
+        return 0;
+    }
+}
